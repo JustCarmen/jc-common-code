24bb9598bcc4c164c2e8d47047ce1533ac7f9d04
JustCarmen <carmen@justcarmen.nl>
2026-05-16 17:30:10 +0200

    Update header

--- autoload.php
+++ autoload.php
@@ -1,28 +1,10 @@
 <?php
 
 /**
- * webtrees: online genealogy
- * Copyright (C) 2025 webtrees development team
- *                    <http://webtrees.net>
+ * justcarmen/jc-common-code:
+ * Library to share common code between custom modules for webtrees by JustCarmen
  *
- * Jefferson49/webtrees-common: Library to share common code between webtrees custom modules
- * Copyright (C) 2025 Markus Hemprich
- *                    <http://www.familienforschung-hemprich.de>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- *
- *
- * Autoload the library for common code between webtrees custom modules
- *
+ * Based on: webtrees-common by Jefferson49 *
  */
 
 $search = str_replace('/', DIRECTORY_SEPARATOR,'/justcarmen/jc-common-code');
