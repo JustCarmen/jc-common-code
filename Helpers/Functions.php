73b5adea81421454953ac9d9c0213de627810699
JustCarmen <carmen@justcarmen.nl>
2026-05-17 13:22:01 +0200

    Remove unused imports

--- Helpers/Functions.php
+++ Helpers/Functions.php
@@ -8,24 +8,11 @@ declare(strict_types=1);
 
 namespace JustCarmen\Webtrees\Helpers;
 
-use Fisharebest\Webtrees\Auth;
-use Fisharebest\Webtrees\Contracts\UserInterface;
-use Fisharebest\Webtrees\Module\ModuleInterface;
 use Fisharebest\Webtrees\Registry;
 use Fisharebest\Webtrees\Webtrees;
-use Fisharebest\Webtrees\Tree;
-use Fisharebest\Webtrees\User;
-use Illuminate\Database\Capsule\Manager as DB;
-use Illuminate\Support\Collection;
-use Illuminate\Database\Query\Builder;
-use Illuminate\Database\Query\JoinClause;
-use Jefferson49\Webtrees\Log\CustomModuleLogInterface;
-
-use Exception;
-
 
 /**
- * Functions to be used in webtrees custom modules
+ * Functions to be used in custom modules for webtrees by JustCarmen
  */
 class Functions
 {
